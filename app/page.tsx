d1a2a276ec65c0777596f9f03d0aab12be73467d
Adi <140542996+AxiomaticAdi@users.noreply.github.com>
2025-10-09 23:27:11 -0700

    chore: title

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center pt-10">
-      <div className="text-2xl font-bold">Admin Actions</div>
+      <div className="text-2xl font-bold">CPF - Admin Actions</div>
       <div className="flex flex-col items-center justify-center pt-10 gap-2">
         <Button asChild>
           <Link href="/create-event">Create Event</Link>
