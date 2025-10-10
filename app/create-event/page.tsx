e38b9dd9db3264c161a94f8c98df21a5256439a1
Adi <140542996+AxiomaticAdi@users.noreply.github.com>
2025-10-09 23:22:57 -0700

    feat: updated homepage

---
+++ app/create-event/page.tsx
@@ -0,0 +1,9 @@
+import { CreateEventForm } from "@/components/create-event-form";
+
+export default function CreateEventPage() {
+  return (
+    <div>
+      <CreateEventForm />
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
-import Image from "next/image";
-import { CreateEventForm } from "@/components/create-event-form";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
-      <CreateEventForm />
+    <div className="flex flex-col items-center justify-center pt-10">
+      <div className="text-2xl font-bold">Admin Actions</div>
+      <div className="flex flex-col items-center justify-center pt-10 gap-2">
+        <Button asChild>
+          <Link href="/create-event">Create Event</Link>
+        </Button>
+      </div>
     </div>
   );
 }
